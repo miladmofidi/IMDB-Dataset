1af44961de33dd087465eb22b80324ea3cf6abf7
Milad Mofidi <miladm@amdocs.com>
2023-01-06 14:41:58 +0300

    Refactoring the project's name

--- src/main/java/com/example/loboximdb/ImdbApplication.java
+++ src/main/java/com/example/loboximdb/ImdbApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author milad.mofidi@gmail.com
  */
 @SpringBootApplication
-public class LoboxImdbApplication
+public class ImdbApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(LoboxImdbApplication.class, args);
+        SpringApplication.run(ImdbApplication.class, args);
     }
 }
